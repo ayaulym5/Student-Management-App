dac23b8daf75061a05305d555f8aacfd6fd6b689
ayaulym5 <168899657+ayaulym5@users.noreply.github.com>
2025-03-02 23:22:05 +0500

    Update AlternativeStudentService.java
    Fixed Alternative

--- src/main/java/com/example/AlternativeStudentService.java
+++ src/main/java/com/example/AlternativeStudentService.java
@@ -1,8 +1,12 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+
+@Service
+@Qualifier("Alternative")
 public class AlternativeStudentService implements StudentService {
     private final StudentRepository repository;
 
