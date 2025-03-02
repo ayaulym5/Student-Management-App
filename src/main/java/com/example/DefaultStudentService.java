a88e16327dd11eec311bc0ccfad8cb8ac72ee0f8
ayaulym5 <168899657+ayaulym5@users.noreply.github.com>
2025-03-02 23:23:06 +0500

    Update DefaultStudentService.java
    Added Primary annotation

--- src/main/java/com/example/DefaultStudentService.java
+++ src/main/java/com/example/DefaultStudentService.java
@@ -1,9 +1,11 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class DefaultStudentService implements StudentService {
     private final StudentRepository repository;
 
